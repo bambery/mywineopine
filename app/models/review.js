4ad651b47d3c22a161b5d25676531342b949a613
bambery <lwszolek@gmail.com>
2016-08-10 20:16:11 -0700

    review model to match api

--- app/models/review.js
+++ app/models/review.js
@@ -1,17 +1,20 @@
 import Model from 'ember-data/model';
-// import attr from 'ember-data/attr';
+import attr from 'ember-data/attr';
 // import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
-  vineyard: DS.attr('string'),
+  producer: DS.attr('string'),
   // vineyard should be moved over to producer, which is vineyard for wines 
   // and company for sodas
-  year: DS.attr('string'),
+  year: DS.attr('number'),
   photo: DS.attr('string'),
 //  type: DS.belongsTo('wineType'),
   wineType: DS.attr('string'),
-  price: DS.attr('string'),
+  price: DS.attr('number'),
   regionOrCity: DS.attr('string'),
   stateOrProvince: DS.attr('string'),
-  country: DS.attr('string')
+  country: DS.attr('string'),
+  avgRating: DS.attr('number'),
+  ratingLauren: DS.attr('number'),
+  ratingDean: DS.attr('number')
 });
